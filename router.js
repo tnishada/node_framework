1ecab015f421b68cd5cc505cc230461cb7df5ba2
U-ALLION\tharindu <tharindu@AT-DT-SLS-04.alliontechnologies.local>
2015-01-21 12:13:34 +0530

    router.js created and modularized the code

---
+++ router.js
@@ -0,0 +1,37 @@
+/**
+ * Created by tharindu on 1/21/2015.
+ */
+var router = {
+    routesArray : {},
+    route : function(req , res){
+
+        //handle favicon url by the routing function itself
+        if (req.url === '/favicon.ico') {
+            res.writeHead(200, {'Content-Type': 'image/x-icon'} );
+            res.end();
+            console.log('favicon requested');
+            return;
+        }
+
+        try{
+            responseObj.response = res;
+            this.routesArray[req.url](req , responseObj);
+        }
+        catch(err){
+            console.log("requested url not found");
+        }
+    }
+};
+
+var responseObj = {
+    response : Object,
+    send : function(responseMessage){
+        this.response.writeHead(200, { 'Content-Type': 'text/plain' });
+        this.response.end(responseMessage);
+    }
+    //sendFile()
+    //sendJSON()
+    //etc
+};
+
+module.exports.router = router;
--- tns.js
+++ tns.js
@@ -1,58 +1,28 @@
 var app = this;
 
-var routes = {};
-
+var router = require('./router.js').router;
 var http = require('http');
 var http_IP = '127.0.0.1';
 
-/*default values for testing*/
-var http_port = 3000;
+//var routes = {}; // array used to store url and callback function
 
 var server = http.createServer(function (req, res) {
-
-    //handle favicon url by the server itself
-    if (req.url === '/favicon.ico') {
-        res.writeHead(200, {'Content-Type': 'image/x-icon'} );
-        res.end();
-        console.log('favicon requested');
-        return;
-    }
-
-    try{
-            responseObj.response = res;
-            routes[req.url](req , responseObj);
-    }
-    catch(err){
-        console.log("requested url not found");
-    }
-
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello, world!\n');
+    router.route(req,res);
 });
 
 app.get = function(urlComponent , callback){
-    routes[urlComponent] = callback;
+    router.routesArray[urlComponent] = callback;
 };
 
 app.listen = function(port , callback){
-    http_port = port;
-    server.listen(http_port,http_IP);
+
+    server.listen(port,http_IP);
     callback();
 };
 
 module.exports.app = app;
 
 
-var responseObj = {
-    response : Object,
-    send : function(responseMessage){
-        this.response.writeHead(200, { 'Content-Type': 'text/plain' });
-        this.response.end(responseMessage);
-    }
-    //sendFile()
-    //sendJSON()
-    //etc
-};
 
 
 
